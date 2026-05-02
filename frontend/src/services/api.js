4ea178820532bcb3410b90dafaeb978c4dfb0815
Anshul Agarwal <anshul.agarwal@newgensoft.com>
2026-05-03 00:29:19 +0530

    Modified Api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://render.com/docs/web-services#port-binding",
+  baseURL: "https://stockflow-mvp-tzvv.onrender.com",
 });
 
 // Attach token if logged in
